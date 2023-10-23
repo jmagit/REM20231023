6b670f332c7b2068b82ece6a8c69b9ef0d34a204
Javier <jmartin@grupoloyal.com>
2023-10-23 19:00:10 +0200

    Inicio

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo");
+		System.out.println("Aplicación arracada ...");
 	}
 
 }
